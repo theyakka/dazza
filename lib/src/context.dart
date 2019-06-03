32a9c5dac04e8105d475d6a602dc2656018de14e
Luke Freeman <freeman.luke@gmail.com>
2019-06-03 09:49:07 -0700

    fix context bug. add extra context build / add methods. add additional tests

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -7,6 +7,8 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'dart:collection';
+
 import 'conversions.dart';
 import 'parameters.dart';
 import 'value_store.dart';
@@ -23,14 +25,19 @@ class Context {
   /// Create an empty [Context].
   Context.empty();
 
-  /// Create a [Context] object and populate it with some initial values
+  /// Create a [Context] object and populate it with one initial value
   Context.initial(String key, dynamic value)
       : assert(key != null),
         assert(value != null) {
     _valueStore[key] = value;
   }
 
-  /// Create a new [Context] from an existing [Context] and append [value] to
+  /// Create a [Context] object and populate it with some initial values
+  Context.initialValues(Map<String, dynamic> values) : assert(values != null) {
+    _valueStore.values.addAll(values);
+  }
+
+  /// Create a new [Context] from an existing [Context] and append a value to
   /// the existing values
   Context.withValue(Context parent, String key, dynamic value)
       : assert(parent != null),
@@ -38,6 +45,15 @@ class Context {
         assert(value != null) {
     _valueStore.setAll(parent._valueStore);
     _valueStore[key] = value;
+  }
+
+  /// Create a new [Context] from an existing [Context] and append multiple
+  /// values to the existing values
+  Context.withValues(Context parent, Map<String, dynamic> values)
+      : assert(parent != null),
+        assert(values != null) {
+    _valueStore.setAll(parent._valueStore);
+    _valueStore.values.addAll(values);
   }
 
   /// Merge two [Context] objects. The parent context will be overridden
@@ -75,22 +91,36 @@ class Context {
   /// Get the context value for the provided [key] and convert it to an [int]
   int intValue(String key) => Conversion.intValue(_valueStore[key]);
 
+  /// Convenience method for creating a new context using the current [Context]
+  /// as the parent. See [Context.withValue].
+  Context withValue(String key, dynamic value) =>
+      Context.withValue(this, key, value);
+
+  /// Convenience method for creating a new context using the current [Context]
+  /// as the parent. See [Context.withValues].
+  Context withValues(Map<String, dynamic> valuePairs) =>
+      Context.withValues(this, values);
+
   /// Convenience getter for the parameters value
   Parameters get parameters =>
       _valueStore[contextKeyParameters] ?? Parameters();
 
+  /// Add a set of parameters to the existing (if any) set of parameters
+  /// defined within this context.
   Parameters addParameters(Parameters parametersToAdd) {
-    final currentParameters = parameters;
-    if (currentParameters == null) {
-      return Parameters();
+    var currentParameters = parameters;
+    if (currentParameters != null) {
+      currentParameters.addParameters(parametersToAdd);
+    } else {
+      currentParameters = parametersToAdd;
     }
-    currentParameters.addParameters(parametersToAdd);
     _valueStore[contextKeyParameters] = currentParameters;
     return currentParameters;
   }
 
-  /// Resets the context values
+  /// Resets the context values.
   void reset() => _valueStore.clear();
 
+  /// Check to see if a parameter is defined.
   bool has(String key) => _valueStore.has(key);
 }
--- test/context_test.dart
+++ test/context_test.dart
@@ -12,10 +12,35 @@ import 'package:test/test.dart';
 
 void main() {
   test('Check that context is being created', () {
-    final Context context = Context.initial('hello', 'world');
-    final Context context2 =
-        Context.withValue(context, 'testing', 'this thing');
+    final context = Context.initial('hello', 'world');
+    final context2 = Context.withValue(context, 'testing', 'this thing');
     print(context.values.length);
     print(context2.values.length);
+  });
+
+  test('Check context value exclusion works', () {
+    var context = Context.initialValues(<String, dynamic>{
+      "testing": "hello",
+      "color": 55,
+      "base": "this thing",
+      "blah": "blahblahblah",
+    });
+    final excludedContext = Context.withoutValue(context, 'blah');
+    expect(excludedContext.has('blah'), isFalse);
+  });
+
+  test('Check typed accessors', () {
+    var context = Context.initialValues(<String, dynamic>{
+      'testing': 'hello',
+      'color': 55,
+      'isreal': true,
+      'numstring': 60,
+      'boolstring': 'true',
+    });
+    expect(context.boolValue('isreal'), isTrue);
+    expect(context.intValue('color'), 55);
+    expect(context.stringValue('numstring'), '60');
+    expect(context.stringValue('testing'), 'hello');
+    expect(context.boolValue('boolstring'), isTrue);
   });
 }
